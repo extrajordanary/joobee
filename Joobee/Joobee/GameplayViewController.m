24cf82b4d729c476e4c4d9609f57321010fc4eb6
Jordan Arnesen <jordan101112@gmail.com>
2014-10-11 19:07:17 -0700

    calculates which team is gaining control over flag and posts new controlStatus

--- Joobee/Joobee/GameplayViewController.m
+++ Joobee/Joobee/GameplayViewController.m
@@ -29,8 +29,8 @@
 
 static NSString* const kBaseURL = @"https://blistering-heat-4085.firebaseio.com/";
 static NSString* const kGameplay = @"GameSession/Gameplay/";
+static NSString* const kGameState = @"https://blistering-heat-4085.firebaseio.com/GameSession/Gameplay/"; // = gameState
 static NSString* const kFlags = @"https://blistering-heat-4085.firebaseio.com/GameSession/Gameplay/Flags/";
-
 
 
 @implementation GameplayViewController {
@@ -47,7 +47,7 @@ static NSString* const kFlags = @"https://blistering-heat-4085.firebaseio.com/Ga
     thisPlayer = @{ playerName : playerName, };
     myTeam = @"Team1";
     
-    [self subscribeToFlagUpdates];
+    [self subscribeToGameUpdates];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -55,9 +55,9 @@ static NSString* const kFlags = @"https://blistering-heat-4085.firebaseio.com/Ga
     // Dispose of any resources that can be recreated.
 }
 
--(void)subscribeToFlagUpdates {
-    Firebase *ref = [[Firebase alloc] initWithUrl:kFlags];
-    [ref observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+-(void)subscribeToGameUpdates {
+    Firebase *gameRef = [[Firebase alloc] initWithUrl:kGameState];
+    [gameRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
         gameState = snapshot.value;
         NSLog(@"Game State: %@", gameState);
     } withCancelBlock:^(NSError *error) {
@@ -74,17 +74,21 @@ static NSString* const kFlags = @"https://blistering-heat-4085.firebaseio.com/Ga
     [self removeSelfFromFlag:1];
 }
 
+- (IBAction)updateGameState:(id)sender {
+    [self updateGameState];
+}
+
 #pragma mark - Flags
 // methods used by all players
 -(void)addSelfToFlag:(int)flagNumber {
     // make call to FB and add self to Flag#, NearbyPlayers, [myTeam]
-    NSString *url = [NSString stringWithFormat:@"%@/Flag%i/NearbyPlayers/%@",kFlags,flagNumber,myTeam];
+    NSString *url = [NSString stringWithFormat:@"%@Flag%i/NearbyPlayers/%@",kFlags,flagNumber,myTeam];
     Firebase* nearFlag = [[Firebase alloc] initWithUrl:url];
     [nearFlag updateChildValues:thisPlayer];
 }
 
 -(void)removeSelfFromFlag:(int)flagNumber {
-    NSString *url = [NSString stringWithFormat:@"%@/Flag%i/NearbyPlayers/%@",kFlags,flagNumber,myTeam];
+    NSString *url = [NSString stringWithFormat:@"%@Flag%i/NearbyPlayers/%@",kFlags,flagNumber,myTeam];
     Firebase* nearFlag = [[Firebase alloc] initWithUrl:url];
     Firebase* leaveFlag = [nearFlag childByAppendingPath:[NSString stringWithFormat:@"/%@",playerName]];
     [leaveFlag removeValue];
@@ -105,15 +109,28 @@ static NSString* const kFlags = @"https://blistering-heat-4085.firebaseio.com/Ga
     // get net difference (Team1 - Team2)
     // positive number means Team1 gains control, negative Team2, 0 no change
     // add net diff to flagStatus
+    NSString *theFlag = [NSString stringWithFormat:@"Flag%i",flagNumber];
+    int team1Count = 0;
+    int team2Count = 0;
     for (int i = 1; i < 3; i++) {
-        NSString *url = [NSString stringWithFormat:@"%@/Flag%i/NearbyPlayers/Team%i",kFlags,flagNumber,i];
-        Firebase *ref = [[Firebase alloc] initWithUrl:url];
-        [ref observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
-            NSLog(@"Team %i Players Near Flag %i: %@", i, flagNumber, snapshot.value);
-        } withCancelBlock:^(NSError *error) {
-            NSLog(@"%@", error.description);
-        }];
+//        (int)[[gameState[@"Flags"][@"Flag1"][@"NearbyPlayers"][@"Team1"] allKeys] count]
+        NSString *theTeam = [NSString stringWithFormat:@"Team%i",i];
+        NSDictionary *nearbyPlayers = gameState[@"Flags"][theFlag][@"NearbyPlayers"][theTeam];
+        int count = (int)[[nearbyPlayers allKeys] count];
+        if (i == 1) {
+            team1Count = count;
+        } else {
+            team2Count = count;
+        }
     }
+    int netDiff = team1Count - team2Count;
+    int currentControlStatusValue = [[gameState[@"Flags"][theFlag] objectForKey:@"ControlStatus"] intValue];
+    NSNumber *newControlStatusValue = @(currentControlStatusValue + netDiff);
+    NSDictionary *newControlStatus = @{ @"ControlStatus" : newControlStatusValue };
+    
+    NSString *url = [NSString stringWithFormat:@"%@Flag%i",kFlags,flagNumber];
+    Firebase *updateFlag = [[Firebase alloc] initWithUrl:url];
+    [updateFlag updateChildValues:newControlStatus];
 }
 
 -(void)updateFlagControl:(int)flagNumber {
